61c1e14db0497ad6ac65ef57aee4daec4b68e4d7
Lucas G. Giachetto <157424638+LucasGGiachetto@users.noreply.github.com>
2025-10-17 16:42:49 -0300

    Add files via upload

--- back/src/routes.js
+++ back/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const Professor = require('./controllers/professores');
+const Turma = require('./controllers/turmas');
 
 routes.get('/', (req, res) => {
     res.send('API Escola Respondendo');
@@ -10,7 +11,13 @@ routes.get('/', (req, res) => {
 
 routes.post('/professor', Professor.create);
 routes.get('/professor', Professor.read);
+routes.delete('/professor/:id', Professor.delete);
+routes.put('/professor/:id', Professor.update);
 
 
+routes.post('/turma', Turma.create);
+routes.get('/turmas', Turma.read);
+routes.delete('/turma/:id', Turma.delete);
+routes.put('/turma/:id', Turma.update);
 
 module.exports = routes;
