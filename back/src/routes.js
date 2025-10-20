aec20e4c4a43c5d678f1811dd4b7227dd8b05c8c
Lucas G. Giachetto <157424638+LucasGGiachetto@users.noreply.github.com>
2025-10-20 15:42:14 -0300

    Add files via upload

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,23 +1,28 @@
+// routes.js
 const express = require('express');
-
 const routes = express.Router();
 
 const Professor = require('./controllers/professores');
 const Turma = require('./controllers/turmas');
+// Importa as funções específicas do controller de atividades
+const AtividadeController = require('./controllers/atividades');
 
 routes.get('/', (req, res) => {
-    res.send('API Escola Respondendo');
+    res.json({titulo:'API Escola Respondendo'});
 });
 
 routes.post('/professor', Professor.create);
 routes.get('/professor', Professor.read);
-routes.delete('/professor/:id', Professor.delete);
-routes.put('/professor/:id', Professor.update);
+routes.post('/login', Professor.login);
 
-
-routes.post('/turma', Turma.create);
 routes.get('/turmas', Turma.read);
-routes.delete('/turma/:id', Turma.delete);
-routes.put('/turma/:id', Turma.update);
+routes.get('/turmas/:id', Turma.read);
+routes.post('/turmas', Turma.create);
+routes.delete('/turmas/:id', Turma.del);
+
+// Registra as funções específicas para cada rota
+routes.get('/atividades/turma/:id', AtividadeController.getAtividadesPorTurma); // <-- Função específica para GET
+routes.get('/atividades/turma', AtividadeController.getAtividadesPorTurma);    // <-- Mesma função para GET sem id
+routes.post('/atividades/nova', AtividadeController.createAtividade);       // <-- Função específica para POST
 
 module.exports = routes;
